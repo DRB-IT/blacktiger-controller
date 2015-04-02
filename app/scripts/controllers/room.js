520c982684cf8a4de5d724037fa6fb1d9b169ad8
Michael Krog <mic@apaq.dk>
2015-04-02 14:15:06 +0200

    Fixed #226. Added logs on login

--- app/scripts/controllers/room.js
+++ app/scripts/controllers/room.js
@@ -8,8 +8,40 @@
  * Controller of the blacktiger-app
  */
 angular.module('blacktiger-controllers')
-        .controller('RoomCtrl', function ($scope, IssuesSvc) {
+        .controller('RoomCtrl', function ($scope, IssuesSvc, LoginSvc) {
+            $scope.initLogs = [];
+            $scope.connected = false;
+    
             $scope.getIssues = function() {
                 return IssuesSvc.getIssues();
             };
+            
+            if(LoginSvc.getCurrentUser()) {
+                $scope.initLogs.push('Logged in correctly.');
+            }
+            
+            $scope.$on('PushEventSvc.Initializing', function() {
+                $scope.initLogs.push('Connecting to Websocket.');
+            });
+            
+            $scope.$on('PushEventSvc.WebsocketConnected', function() {
+                $scope.initLogs.push('Has successfully connected to Websocket.');
+            });
+            
+            $scope.$on('PushEventSvc.ResolvingRooms', function() {
+                $scope.initLogs.push('Resolving accessible rooms.');
+            });
+            
+            $scope.$on('PushEventSvc.SubscribingEvents', function() {
+                $scope.initLogs.push('Subscribing to events.');
+            });
+            
+            $scope.$on('PushEventSvc.NoRooms', function() {
+                $scope.initLogs.push('No rooms accessible for user.');
+            });
+            
+            $scope.$on('PushEventSvc.Initialized', function() {
+                $scope.initLogs.push('Connected.');
+                $scope.connected = true;
+            });
         });
