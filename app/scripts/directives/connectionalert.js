6d26a98becccd4ba2d12515f22d4ec8fa313978c
Michael Krog <mic@apaq.dk>
2015-05-28 13:20:02 +0200

    Fixed #191. Added countdown when reconnecting.

--- app/scripts/directives/connectionalert.js
+++ app/scripts/directives/connectionalert.js
@@ -10,15 +10,40 @@ angular.module('blacktiger-directives')
         .directive('btConnectionAlert', function () {
             return {
                 restrict: 'E',
-                controller: function ($scope) {
+                controller: function ($scope, $interval) {
                     $scope.visible = false;
 
+                    $scope.stopCounter = function() {
+                        if(angular.isDefined($scope.countdownInterval)) {
+                            $interval.cancel($scope.countdownInterval);
+                        }
+                        delete $scope.countdownInterval;
+                    };
+                    
+                    $scope.startCounter = function() {
+                        $scope.stopCounter();
+                        
+                        $scope.countdown = 10;
+                        $scope.countdownInterval = $interval(function() {
+                            $scope.countdown--;
+                        }, 1000);
+                    };
+                    
                     $scope.$on('PushEventSvc.Lost_Connection', function() {
                         $scope.visible = true;
+                        $scope.startCounter();
                     });
                     
                     $scope.$on('PushEventSvc.Initialized', function() {
                         $scope.visible = false;
+                    });
+                    
+                    $scope.$on('PushEventSvc.Reconnecting', function() {
+                        $scope.startCounter();
+                    });
+                    
+                    $scope.$on('$destroy', function() {
+                        $scope.stopCounter();
                     });
                     
                 },
