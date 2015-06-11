9be61a75524992240bcb7ea6a00df82cf943d129
Michael Krog <mic@apaq.dk>
2015-06-11 13:42:10 +0200

    Fixed code style. Removed on regexp replace in language normalization.

--- app/scripts/controllers/menu.js
+++ app/scripts/controllers/menu.js
@@ -71,15 +71,14 @@ angular.module('blacktiger-controllers')
             
             $scope.normalizeLanguageName = function(name) {
                         var r=name.toLowerCase();
-                        r = r.replace(new RegExp("[àáâãäå]", 'g'),"a");
-                        r = r.replace(new RegExp("ç", 'g'),"c");
-                        r = r.replace(new RegExp("[èéêë]", 'g'),"e");
-                        r = r.replace(new RegExp("[ìíîï]", 'g'),"i");
-                        r = r.replace(new RegExp("ñ", 'g'),"n");                            
-                        r = r.replace(new RegExp("[òóôõö]", 'g'),"o");
-                        r = r.replace(new RegExp("œ", 'g'),"oe");
-                        r = r.replace(new RegExp("[ùúûü]", 'g'),"u");
-                        r = r.replace(new RegExp("[ýÿ]", 'g'),"y");
+                        r = r.replace(new RegExp('[àáâãäå]', 'g'),'a');
+                        r = r.replace(new RegExp('ç', 'g'),'c');
+                        r = r.replace(new RegExp('[èéêë]', 'g'),'e');
+                        r = r.replace(new RegExp('[ìíîï]', 'g'),'i');
+                        r = r.replace(new RegExp('ñ', 'g'),'n');                            
+                        r = r.replace(new RegExp('[òóôõö]', 'g'),'o');
+                        r = r.replace(new RegExp('[ùúûü]', 'g'),'u');
+                        r = r.replace(new RegExp('[ýÿ]', 'g'),'y');
                         return r;
             };
 
